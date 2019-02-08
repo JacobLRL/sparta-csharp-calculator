a87d0f0e9852bec13d40e8d39619fc900202e718
JacobLRL <JLittle@spartaglobal.com>
2019-02-08 11:26:36 +0000

    sorted out control of buttons

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -23,6 +23,8 @@ namespace WpfCalculator
         static int arg1 = 0;
         static int arg2 = 0;
         static string opt = "";
+        static bool arg1Entered = false;
+        static bool opEntered = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -31,19 +33,27 @@ namespace WpfCalculator
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Screen.Text += (sender as Button).Content;
+            arg1Entered = true;
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
             Screen.Text = "";
+            arg1 = 0;
+            arg2 = 0;
+            opt = "";
+            arg1Entered = false;
+            opEntered = false;
         }
 
         private void Operator_Click(object sender, RoutedEventArgs e)
         {
-            arg1 = int.Parse(Screen.Text);
-            opt = (sender as Button).Content.ToString();
-            Screen.Text += opt;
-            
+            if (arg1Entered) {
+                arg1 = int.Parse(Screen.Text);
+                opt = (sender as Button).Content.ToString();
+                Screen.Text += opt;
+                opEntered = true;
+            }
         }
 
         private void Enter_Click(object sender, RoutedEventArgs e)
@@ -51,8 +61,19 @@ namespace WpfCalculator
             string txtOnScrn = Screen.Text;
             int index = txtOnScrn.IndexOf(opt);
             string ag2 = txtOnScrn.Substring(index);
-            arg2 = int.Parse(txtOnScrn.Substring(index+1));
+            if (opEntered)
+            {
+                arg2 = int.Parse(txtOnScrn.Substring(index + 1));
+            }
+            else
+            {
+                arg1 = int.Parse(Screen.Text); 
+            }
+            
             Screen.Text = Operation(opt, arg1, arg2).ToString();
+
+            opEntered = false;
+            arg2 = 0;
         }
 
         public int Operation(string op, int x, int y) {
@@ -67,7 +88,7 @@ namespace WpfCalculator
                 case "/":
                     return x / y;
                 default:
-                    return 0;
+                    return x;
             }
         }
 
